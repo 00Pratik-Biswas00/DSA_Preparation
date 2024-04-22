cd46085c27a79527603d6ebca320a152df50aaf6
Pratik Biswas <04biswaspratik@gmail.com>
2024-04-22 12:01:36 +0530

    709. To Lower Case

---
+++ java/709. To Lower Case.java
@@ -0,0 +1,10 @@
+class ToLowerCase {
+  public static String toLowerCase(String s) {
+    return s.toLowerCase();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(toLowerCase("Hello"));
+
+  }
+}
