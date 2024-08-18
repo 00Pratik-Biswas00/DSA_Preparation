9f384d4f6176b3e2972d9ba46a4d9b3a68c49363
Pratik Biswas <04biswaspratik@gmail.com>
2024-08-18 13:37:39 +0530

    55. Jump Game

---
+++ java/HouseRobber.java
@@ -0,0 +1,11 @@
+public class HouseRobber {
+  public static int rob(int[] nums) {
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int nums[] = { 2, 3, 1, 1, 4 };
+    System.out.println(rob(nums));
+  }
+}
