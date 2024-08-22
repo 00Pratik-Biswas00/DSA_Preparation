ca03c5a9b5ad2337a88316e2f749be672d2db5e5
Pratik Biswas <04biswaspratik@gmail.com>
2024-08-22 15:27:45 +0530

    66. Plus One

--- java/PlusOne.java
+++ java/PlusOne.java
@@ -1,12 +1,24 @@
 public class PlusOne {
-  public int[] plusOne(int[] digits) {
+  public static int[] plusOne(int[] digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+
+    digits = new int[digits.length + 1];
+    digits[0] = 1;
+
     return digits;
   }
 
   public static void main(String[] args) {
-    int[] points = { 1, 2, 3 };
-    for (int i : points) {
-      System.out.println(i + " ");
+    int[] points = { 4, 9 };
+    int res[] = plusOne(points);
+    for (int i : res) {
+      System.out.print(i + " ");
     }
   }
 }
