d77c62e61b7fa878933fad79f1b64e2eec771239
Pratik Biswas <04biswaspratik@gmail.com>
2024-08-23 19:24:03 +0530

    1. EmailValidity

---
+++ interview_preparation/1. EmailValidity.java
@@ -0,0 +1,24 @@
+// To verify whether a given input string is a valid e-mail id match it with the following is the regular expression to match an e-mail id −
+
+// "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+
+package interview_preparation;
+
+import java.util.Scanner;
+
+class EmailValidity {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your Email: ");
+    String phone = sc.next();
+    String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+    // Matching the given phone number with regular expression
+    boolean result = phone.matches(regex);
+    if (result) {
+      System.out.println("Given email-id is valid");
+    } else {
+      System.out.println("Given email-id is not valid");
+    }
+    sc.close();
+  }
+}
