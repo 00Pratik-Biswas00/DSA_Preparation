1be61e233928772d9ab9668195d0b6dea3d2975e
Pratik Biswas <04biswaspratik@gmail.com>
2024-08-23 20:21:10 +0530

    2. TicketEncodingSystem

--- interview_preparation/2. TicketEncodingSystem.java
+++ interview_preparation/2. TicketEncodingSystem.java
@@ -1,3 +1,28 @@
+// Ticket Encoding Sequence
+// Last Updated : 09 Dec, 2023
+// You are working at a ticketing company that generates unique ticket codes for various events. You have given a number N and your task is to print the Nth ticket code. The ticket codes are generated based on a specific encoding sequence. The encoding sequence follows the recursive formula as described below:
+
+// The ticket code for the first ticket (ticket #1) is “A.”
+// Now, to generate rth ticket you have to take an r-1 th ticket and create a new encoding by writing down the frequency followed by a digit.
+// for example Ticket#(r-1) = “1121A” (Two 1, One 2, One 1, One A) so Ticket#(r) = “2112111A”
+// Examples:
+
+// Input: 4
+// Output: 311A
+// Explanation:
+
+// Ticket #1: “A”
+// Ticket #2: Use “A” => one A => “1A”
+// Ticket #3: Use “1A” => one 1 + one A => “111A”
+// Ticket #4: Use “111A” => three 1 + one A => “311A”
+// Input: 3
+// Output: 111A
+// Explanation:
+
+// Ticket #1: “A”
+// Ticket #2: Use “A” => one A => “1A”
+// Ticket #3: Use “1A” => one 1 + one A => “111A”
+
 package interview_preparation;
 
 import java.util.Scanner;
